e48764e6b3f3f210b5398888207d87a3c58efd20
lillie <amberisamab@gmail.com>
2023-10-20 15:12:07 +1300

    whoops forgot to include js in the commit :)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
 import {EditorView, minimalSetup} from "codemirror"
+import {EditorState} from "@codemirror/state"
 import {markdown} from "@codemirror/lang-markdown"
 
-let view = new EditorView({
-  extensions: [markdown(), minimalSetup, EditorView.lineWrapping],
-  parent: document.getElementById("editor"),
-  fontSize: 20,
+let root = document.querySelector(":root");
+
+let sync_val = "";
+
+function char_count(e) {
+  let chars = 0;
+
+  e.split("").forEach((e) => {
+    chars += 1;
+  });
+  document.getElementById("charcount").innerText = chars + " characters";
+}
+
+let state = EditorState.create({
+  extensions: [
+    markdown(), 
+    minimalSetup, 
+    EditorView.lineWrapping,
+    EditorView.updateListener.of(function(e) {
+      char_count(e.state.doc.toString());
+    })
+  ]
 })
+
+let view = new EditorView({
+  state: state,
+  parent: document.getElementById("editor"),
+})
+
+document.getElementById("fsize").addEventListener("click", () => {
+  console.log("cicket");
+});
